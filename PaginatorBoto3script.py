2748f6239bf96f017e7829ad247447981c47f6d1
lohitj <39816251+lohitj@users.noreply.github.com>
2019-10-26 21:47:49 +0530

    Update PaginatorBoto3script.py

--- PaginatorBoto3script.py
+++ PaginatorBoto3script.py
@@ -11,9 +11,9 @@ sheet1.write(0,1,'Key')
 sheet1.write(0,2,'Metadata')
 client = boto3.client('s3')
 paginator = client.get_paginator('list_objects_v2').paginate(
-        Bucket='elasticbeanstalk-ap-south-1-100931169541'
+        Bucket='elasticbeanstalk-ap-south-1-100931169541',
+        PaginationConfig={'MaxItems': 5000}
         )
-i=1.0
 for page in paginator:
     for objct in page['Contents']:
         response = client.get_object(
@@ -23,9 +23,4 @@ for page in paginator:
         sheet1.write(int(i),0,objct['ETag'])
         sheet1.write(int(i),1,objct['Key'])
         sheet1.write(int(i),2,str(response['Metadata']))
-        i = i+1
-        if i==5000:
-            break
-        else:
-            continue
 wb.save("Etag3_example.xls")
